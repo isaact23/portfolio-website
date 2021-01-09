f30a793979a21dfa4c17c2e61433503da959d030
shufflesninja <isaact23@vt.edu>
2021-01-09 13:40:26 -0500

    Add Unity3D showcase section

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import hackDuke from '../img/hackduke.png';
-import hackUMass from '../img/hackumass.png';
-import newYearNewHack from '../img/nynh.png';
+import personalImage from '../img/isaacthompson.jfif';
+import hackDuke from '../img/hackathons/hackduke.png';
+import hackUMass from '../img/hackathons/hackumass.png';
+import newYearNewHack from '../img/hackathons/nynh.png';
+import iTakeStew from '../img/unity/itakestew.png';
+import flopp from '../img/unity/flopp.png';
+import stackCity from '../img/unity/stackcity.png';
 
 class Home extends React.Component {
 
@@ -12,10 +16,40 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <div class="card" id="header">
-                    <h1>Isaac Thompson's Website</h1>
+                <div className="card" id="intro">
+                    <div className="introText">
+                        <h1>Welcome!</h1>
+                        <p>This is Isaac Thompson's personal website.</p>
+                    </div>
+                    <div className="introImage">
+                        <img src={personalImage} />
+                    </div>
                 </div>
-                <div class="card" id="skills">
+                <div className="card" id="unity">
+                    <h1>Unity3D showcase</h1>
+                    <a href="https://gaming-project-at-vt.itch.io/i-take-stew" target="_blank" rel="noreferrer">
+                        <div className="unityImg"><img src={iTakeStew} alt="I Take Stew"/></div>
+                    </a>
+                    <a href="https://shufflesninja.itch.io/flopp" target="_blank" rel="noreferrer">
+                        <div className="unityImg"><img src={flopp} alt="Flopp"/></div>
+                    </a>
+                    <a href="https://shufflesninja.itch.io/stack-city" target="_blank" rel="noreferrer">
+                        <div className="unityImg"><img src={stackCity} alt="Stack City"/></div>
+                    </a>
+                </div>
+                <div className="card" id="hackathons">
+                    <h1>My hackathons</h1>
+                    <a href="https://devpost.com/software/carbon-crushes" target="_blank" rel="noreferrer">
+                        <div className="hackImg"><img src={hackDuke} alt="HackDuke"/></div>
+                    </a>
+                    <a href="https://dashboard.hackumass.com/projects/22" target="_blank" rel="noreferrer">
+                        <div className="hackImg"><img src={hackUMass} alt="HackUMass"/></div>
+                    </a>
+                    <a href="https://devpost.com/software/placeholder-nbyseg" target="_blank" rel="noreferrer">
+                        <div className="hackImg"><img src={newYearNewHack} alt="NewYearNewHack"/></div>
+                    </a>
+                </div>
+                <div className="card" id="skills">
                     <h1>Tools I use</h1>
                     <div className="skillBox" id="python">    <h2>Python</h2></div>
                     <div className="skillBox" id="java">      <h2>Java</h2></div>
@@ -25,18 +59,6 @@ class Home extends React.Component {
                     <div className="skillBox" id="unity">     <h2>Unity3D</h2></div>
                     <div className="skillBox" id="blender">   <h2>Blender</h2></div>
                     <div className="skillBox" id="flutter">   <h2>Flutter</h2></div>
-                </div>
-                <div class="card" id="hackathons">
-                    <h1>My hackathons</h1>
-                    <a href="https://devpost.com/software/carbon-crushes" target="_blank" rel="noreferrer">
-                        <div className="hackImg"><img src={hackDuke}/></div>
-                    </a>
-                    <a href="https://dashboard.hackumass.com/projects/22" target="_blank" rel="noreferrer">
-                        <div className="hackImg"><img src={hackUMass}/></div>
-                    </a>
-                    <a href="https://devpost.com/software/placeholder-nbyseg" target="_blank" rel="noreferrer">
-                        <div className="hackImg"><img src={newYearNewHack}/></div>
-                    </a>
                 </div>
             </div>
         );
